d2fe772db5c6f1cfb9edd9bee0269ecc78c748a5
Steven Rubinstein <slrubinstein@gmail.com>
2017-03-06 15:01:06 -0500

    add jsmpeg and try to read video over socket

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Stream = require('node-rtsp-stream-es6')
 
 const options = {
-  name: 'streamName',
-  // url: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov',
-  url: 'rtsp://admin:hack9000@172.23.2.0:554/cam/realmonitor?channel=1&subtype=0',
+  name: 'lilguy',
+  url: 'rtsp://admin:hack9000@10.11.10.217/cam/realmonitor?channel=1&subtype=0',
   port: 8000
 }
 
