9917bcc5666ce31c990cfae124b438a1ab3dba8b
Daniel Duan <dduan@squarespace.com>
2017-03-07 18:56:13 -0500

    split to utils file

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -194,23 +194,6 @@ onDiffMessage = ({ data }) => {
   difficulty = (difficulty + data.difficulty * 40) / 2;
 };
 
-/**
- * This collision method works better when visibility is generally good and
- * the feed is stable
- */
-detectCollision = () => {
-  const data = dctx.getImageData(frogX, frogY, frogWidth, frogHeight).data;
-  let sum = 0;
-  for (let i=0; i<data.length; i+=8) {
-    sum+=data[i];
-  }
-  return sum > 10000;
-}
-
-detectFly = () => {
-  return (Math.abs(frogX - flyX) < 20 && Math.abs(frogY - flyY) < 20);
-}
-
 drawGame = () => {
   gctx.clearRect(0,0,w,h);
 
---
+++ client/assets/js/utils.js
@@ -0,0 +1,16 @@
+/**
+ * This collision method works better when visibility is generally good and
+ * the feed is stable
+ */
+detectCollision = () => {
+  const data = dctx.getImageData(frogX, frogY, frogWidth, frogHeight).data;
+  let sum = 0;
+  for (let i=0; i<data.length; i+=8) {
+    sum+=data[i];
+  }
+  return sum > 10000;
+}
+
+detectFly = () => {
+  return (Math.abs(frogX - flyX) < 20 && Math.abs(frogY - flyY) < 20);
+}
