3cecb7cbc122b507be2d95a1efd178cd44f5ff83
Tim Miller <tmiller@squarespace.com>
2017-03-07 19:43:27 -0500

    Updates menu styles

--- client/assets/js/menu.js
+++ client/assets/js/menu.js
@@ -1,22 +1,35 @@
 // const body = document.getElementByTagNames('body')[0];
 const menuElement = document.getElementById('menu');
+const levelContainer = document.getElementById('levels');
 let menuVideo;
 
+levels = [
+  {img: 'level0.svg', name: 'Live Stream'},
+  {img: 'level1.png', name: 'Manhattan', src: 'lvl1.mov'},
+  {img: 'level2.png', name: 'Brooklyn', src: 'lvl2.mov'},
+  {img: 'level3.png', name: 'Bronx', src: 'lvl3.mov'},
+];
+
+
 initMenu = () => {
-  body.classList.add('skrim');
-  menuElement.classList.add('visible');
   document.addEventListener('keyup', onKeyUpStart);
-  menuVideo = setInterval(drawVideo, 16);
+  levels.forEach(level => {
+    levelContainer.appendChild(createLevelMarkup(level.img, level.name));
+  });
 }
 
 onKeyUpStart = (e) => {
   if (e.keyCode === 83) {
     document.removeEventListener('keyup', onKeyUpStart);
-    body.classList.remove('skrim');
-    menuElement.classList.remove('visible');
-    clearInterval(menuVideo);
     startGame();
   }
+}
+
+createLevelMarkup = (img, name) => {
+  const level = document.createElement('div');
+  level.classList.add('level');
+  level.innerHTML = `<img class="levelImg" src="../assets/img/${img}"><div>${name}</div>`;
+  return level;
 }
 
 initMenu();
