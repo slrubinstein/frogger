2735a44e28eff7ba34e1fec187bdca6bada413f8
Tim Miller <tmiller@squarespace.com>
2017-03-08 14:25:55 -0500

    game over sound

--- client/assets/js/gameOver.js
+++ client/assets/js/gameOver.js
@@ -1,10 +1,12 @@
 const gameOverScreen = document.getElementById('gameOver');
+const sadNoiseSignalingTheEndOfTheGame = new Audio('assets/sounds/game-over.mp3'),
 
 gameOver = () => {
   isGameOver = true;
   gameOverScreen.style.display = 'flex';
   frogX = null;
   frogY = null;
+  sadNoiseSignalingTheEndOfTheGame.play();
   document.removeEventListener('keyup', onKeyUp);
   document.addEventListener('keyup', onKeyUpRestart);
 }
