d0003efc2f1cc5f1e2f604234b2cef553cd7e96c
Steven Rubinstein <slrubinstein@gmail.com>
2017-03-09 12:22:14 -0500

    add hi scores

--- client/assets/js/gameOver.js
+++ client/assets/js/gameOver.js
@@ -1,6 +1,9 @@
 const gameOverScreen = document.getElementById('gameOver');
 const sadNoiseSignalingTheEndOfTheGame = new Audio('assets/sounds/game-over.mp3');
-const database = firebase.database();
+const hiScoresScreen = document.getElementById('hiScoresScreen');
+const dbHiScores = document.getElementById('dbHiScores');
+const dbRef = firebase.database().ref().child('dbHiScores');
+const hiScores = [];
 
 gameOver = () => {
   trafficNoise.pause();
@@ -11,6 +14,7 @@ gameOver = () => {
   sadNoiseSignalingTheEndOfTheGame.play();
   document.removeEventListener('keydown', onKeyDown);
   document.addEventListener('keyup', onKeyUpRestart);
+  showSavedHiScores();
 }
 
 onKeyUpRestart = (e) => {
@@ -24,3 +28,35 @@ onKeyUpRestart = (e) => {
     document.removeEventListener('keyup', onKeyUpRestart);
   }
 }
+
+showSavedHiScores = () => {
+  hiScoresScreen.style.display = 'block';
+}
+
+saveHiScore = (name, score) => {
+  dbRef.push().set({
+    name,
+    score
+  });
+}
+
+
+createScore = ({ name, score }) => {
+  const container = document.createElement('div');
+  const nameEl = document.createElement('span');
+  nameEl.innerHTML = name;
+  container.append(nameEl);
+  const scoreEl = document.createElement('span');
+  scoreEl.innerHTML = score;
+  scoreEl.classList.add('dbHiScore');
+  container.append(scoreEl);
+  dbHiScores.append(container);
+}
+
+dbRef.orderByValue().limitToLast(10).on('value', (snap) => {
+  snap.forEach((d) => {
+    let val = d.val();
+    hiScores.push({ name: val.name, score: val.score });
+  });
+  hiScores.reverse().forEach(createScore);
+});
