6d862af2e490d60106eb229262255de330b1a25a
Steven Rubinstein <slrubinstein@gmail.com>
2017-03-09 14:20:54 -0500

    rank and enter hi scores

--- client/assets/js/gameOver.js
+++ client/assets/js/gameOver.js
@@ -1,9 +1,18 @@
-const gameOverScreen = document.getElementById('gameOver');
 const sadNoiseSignalingTheEndOfTheGame = new Audio('assets/sounds/game-over.mp3');
-const hiScoresScreen = document.getElementById('hiScoresScreen');
+
+const gameOverScreen = document.getElementById('gameOver');
 const dbHiScores = document.getElementById('dbHiScores');
+const restart = document.getElementById('restart');
+const enterHiScore = document.getElementById('enterHiScore');
+const cursor = document.getElementById('cursor');
+const enterName = document.getElementById('enterName');
+
 const dbRef = firebase.database().ref().child('dbHiScores');
+
 const hiScores = [];
+let name = '';
+
+const MAX_SCORES_LENGTH = 5;
 
 gameOver = () => {
   trafficNoise.pause();
@@ -13,8 +22,44 @@ gameOver = () => {
   frogY = null;
   sadNoiseSignalingTheEndOfTheGame.play();
   document.removeEventListener('keydown', onKeyDown);
+
+  if (!hiScores.length || hiScores.length < MAX_SCORES_LENGTH || score > hiScores.slice(-1)[0].score) {
+    setTimeout(enterNewHiScore, 500);
+    return;
+  }
+  listenForRestart();
+}
+
+enterNewHiScore = () => {
+  cursor.style.display = 'inline-block';
+  enterHiScore.style.display = 'block';
+  document.addEventListener('keyup', nameListener);
+}
+
+
+nameListener = (e) => {
+  if (e.keyCode === 13) {
+    saveHiScore();
+  }
+  const letter = String.fromCharCode(e.keyCode)
+  name += letter;
+  enterName.innerHTML = name;
+
+  if (name.length === 3) {
+    saveHiScore();
+  }
+}
+
+saveHiScore = () => {
+  document.removeEventListener('keyup', nameListener);
+  sendHiScoreToDB();
+  listenForRestart();
+  cursor.style.display = 'none';
+}
+
+listenForRestart = () => {
   document.addEventListener('keyup', onKeyUpRestart);
-  showSavedHiScores();
+  restart.style.display = 'block';
 }
 
 onKeyUpRestart = (e) => {
@@ -29,17 +74,14 @@ onKeyUpRestart = (e) => {
   }
 }
 
-showSavedHiScores = () => {
-  hiScoresScreen.style.display = 'block';
-}
-
-saveHiScore = (name, score) => {
+sendHiScoreToDB = () => {
   dbRef.push().set({
     name,
     score
   });
+  name = '';
+  enterName.innerHTML = '';
 }
-
 
 createScore = ({ name, score }) => {
   const container = document.createElement('div');
@@ -53,10 +95,21 @@ createScore = ({ name, score }) => {
   dbHiScores.append(container);
 }
 
-dbRef.orderByValue().limitToLast(10).on('value', (snap) => {
+dbRef.orderByChild('score').limitToLast(MAX_SCORES_LENGTH).on('value', (snap) => {
+  hiScores.length = 0;
   snap.forEach((d) => {
     let val = d.val();
     hiScores.push({ name: val.name, score: val.score });
   });
+  dbHiScores.innerHTML = '';
   hiScores.reverse().forEach(createScore);
+  if (hiScores[0]) {
+    displayHiScore(hiScores[0].score);
+  }
 });
+
+
+displayHiScore = (score) => {
+  hiScore = score;
+  gameHiScore.innerHTML = score;
+};
